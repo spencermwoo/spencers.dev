301e4eb1006ad049335f9faeb89659283d20234f
spencerwoo <spencer.woo@liferay.com>
2024-09-15 21:22:56 -0400

    use 100vh for text-wrapping mobile for now

--- web/plugins/google-docs.shortcode.js
+++ web/plugins/google-docs.shortcode.js
@@ -1,4 +1,4 @@
 // const outdent = require('outdent')({ newline: ' ' });
 module.exports = (id) => {
-    return `<iframe src="https://docs.google.com/document/d/e/${id}/pub?embedded=true" style="border: none; width: 80%; height: 80vh"></iframe>`;
+    return `<iframe src="https://docs.google.com/document/d/e/${id}/pub?embedded=true" style="border: none; width: 100vh; height: 80vh"></iframe>`;
 };
